1b453ce3567ecfee927d6780207ace98cb9dda5e
Ktexaswalker <55514618+Ktexaswalker@users.noreply.github.com>
2021-06-23 15:02:04 +0200

    Update principal.py

--- principal.py
+++ principal.py
@@ -24,8 +24,8 @@ misfondos = bd.fondos
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
-app.config["MAIL_USERNAME"] = "ktexasw@gmail.com"
-app.config["MAIL_PASSWORD"] = "qwer1234,.-"
+app.config["MAIL_USERNAME"] = "correo@electronico.com"
+app.config["MAIL_PASSWORD"] = "contraseña"
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
 mail = Mail(app)
